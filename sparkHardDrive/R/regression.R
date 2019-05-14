95d978aa7988d525c0c76a892f48fc0382d02109
kklamsi <30492918+kklamsi@users.noreply.github.com>
2019-05-14 20:49:27 +0200

    Add files via upload
    script to train and test the logisic model

---
+++ sparkHardDrive/R/regression.R
@@ -0,0 +1,26 @@
+library(sparklyr)
+library(tidyverse)
+
+sc <- spark_connect(master = "local")
+
+#features for regression:
+#smart_1_normalized, smart_3_normalized, smart_5_normalized,
+#smart_7_normalized, smart_9_normalized, smart_192_normalized,
+#smart_193_normalized, smart_194_normalized
+
+
+## logistic regression
+sample_tbl <- sdf_copy_to(sc, sample, name = "sample", overwrite = TRUE)
+
+partitions <- sample_tbl %>%
+  sdf_partition(training = 0.7, test = 0.3, seed = 1111)
+
+sample_training <- partitions$training
+sample_test <- partitions$test
+
+lr_model <- sample_training %>%
+  ml_logistic_regression(failure ~ smart_1_normalized + smart_3_normalized + smart_5_normalized + smart_7_normalized + smart_9_normalized) # + smart_192_normalized + smart_193_normalized + smart_194_normalized
+
+pred <- ml_predict(lr_model, sample_test)
+
+ml_binary_classification_evaluator(pred)
--- sparkHardDrive/R/trialAndError.R
+++ sparkHardDrive/R/trialAndError.R
@@ -1,4 +1,40 @@
-install.packages("SparkR")
-library(SparkR)
-install.spark()
+library(sparklyr)
+library(tidyverse)
+library(dplyr)
 
+sc <- spark_connect(master = "local")
+
+read.csv.zip <- function(zipfile, ...) {
+  # Create a name for the dir where we'll unzip
+  zipdir <- tempfile()
+
+  # Create the dir using that name
+  dir.create(zipdir)
+  # Unzip the file into the dir
+  unzip(zipfile, exdir=zipdir)
+  # Get a list of csv files in the dir
+  files <- list.files(zipdir)
+  files <- files[grep("//.csv$", files)]
+  # Create a list of the imported csv files
+  csv.data <- sapply(files, function(f) {
+    fp <- file.path(zipdir, f)
+    return(read.csv(fp, ...))
+  })
+  return(csv.data)}
+
+
+data_Q1_2016 <- read.csv.zip("/data_Q1_2016.zip")
+data_Q1_2016 <- read.csv("/data_Q1_2016.csv")
+
+## harddriveKaggle ##
+
+harddriveKaggle <- read.csv("/harddriveKaggle.csv")
+harddriveKaggle <- select(harddriveKaggle, failure, smart_1_normalized, smart_3_normalized,
+                          smart_5_normalized, smart_7_normalized, smart_9_normalized,
+                          smart_192_normalized, smart_193_normalized, smart_194_normalized)
+
+
+sampleFailure <- sample_n(filter(harddriveKaggle, failure == 1), 1000, replace = T)
+sampleRunning <- sample_n(filter(harddriveKaggle, failure == 0), 1000, replace = F)
+
+sample <- bind_rows(sampleFailure, sampleRunning)
